1173640a7dde836ba306048c8171ed6ebc1aa8ec
Emily Williams <emily.marie.williams@protonmail.com>
2025-03-08 04:55:56 -0800

    Removed some commented-out code.

--- new/forex/pre_training_data_prep/pull_forex_data.py
+++ new/forex/pre_training_data_prep/pull_forex_data.py
@@ -1,12 +1,6 @@
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 
-def pull_forex_data(
-        #price_type_name = 'mid',
-        #instrument_name = 'EUR/USD',
-        #interval_name = 'Minute',
-        #output_file_name_and_path = None,
-        **config,
-):
+def pull_forex_data(**config):
     mysql_hook = MySqlHook(mysql_conn_id = config['database_name'])
 
     sql = f"SELECT ts.timestamp, cs.o, cs.l, cs.h, cs.c, v.volume FROM timeseries_candlestick cs, timeseries_instrument inst, timeseries_interval iv, timeseries_pricetype pt, timeseries_volume v, timeseries_timestamp ts WHERE cs.instrument_id = inst.id AND cs.interval_id = iv.id AND cs.price_type_id = pt.id AND cs.volume_id = v.id AND cs.timestamp_id = ts.id AND pt.name = '%s' AND inst.name = '%s' AND iv.name = '%s' ORDER BY timestamp;"
